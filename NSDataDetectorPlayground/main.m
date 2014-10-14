e6b053dae2100b9382f36d2a356658a188ca938e
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-13 19:41:48 -0700

    Initial Commit

---
+++ NSDataDetectorPlayground/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSDataDetectorPlayground
+//
+//  Created by Anthony Dagati on 10/13/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
